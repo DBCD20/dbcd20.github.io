ecea3277ee4a34b069074c3c60f0697a7aa58aae
Dave <dmagsipoc@gmail.com>
2025-08-11 14:31:05 +0000

    update about me: add image

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,7 +11,7 @@ export default function About() {
           {/* Image */}
           <div className="flex-shrink-0">
             <Image
-              src="/my-photo.jpg" // replace with your actual image path
+              src="/my-photo.JPG" // replace with your actual image path
               alt="Profile photo"
               width={300}
               height={300}
