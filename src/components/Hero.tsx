b08abe5e6d0779f4de585fd8784256513ac0880f
Dave <dmagsipoc@gmail.com>
2025-08-11 13:01:11 +0000

    fix video poster

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
       id="hero"
     >
       <BackgroundVideo />
-      <div className="text-center space-y-4 px-4 rounded-lg p-6 relative z-10">
+      <div className="text-center space-y-4 px-4 rounded-lg p-6 relative">
         <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl  text-shadow-lg/70">Hello, I&apos;m David</h1>
         <p className="text-xl text-gray-100 max-w-xl mx-auto  text-shadow-lg/70">
           a passionate DevOps engineer specializing in automation, CI/CD pipelines, and cloud infrastructure. With a focus on efficiency, reliability, and scalability
